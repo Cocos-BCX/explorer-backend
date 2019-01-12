14c3c910ad1cb78ebbae45f8b4ee0ebb4f4bb0a1
zhen.liu <zhen.liu@chukong-inc.com>
2019-01-12 17:56:53 +0800

    db

--- config/db.js
+++ config/db.js
@@ -5,8 +5,8 @@ if (process.env.NODE_ENV === 'released' || process.env.NODE_ENV === 'release') {
 } else if (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'testing') {
 	host = 'mongodb://root:xeJE0jWU@localhost/test'
 } else {
-	// host = 'mongodb://localhost/test'
-    host = 'mongodb://192.168.90.45/test'
+	host = 'mongodb://localhost/test'
+	// host = 'mongodb://192.168.90.45/test'
 }
 
 module.exports = exports = host
