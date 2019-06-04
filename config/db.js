275a42d63afd3819903b61288d169b4c67ebdb9a
ypenghui6 <ypenghui6@163.com>
2019-06-04 10:39:10 +0800

    update mongo db

--- config/db.js
+++ config/db.js
@@ -1,13 +1,13 @@
 let host
 if (process.env.NODE_ENV === 'released' || process.env.NODE_ENV === 'release') {
-	host = 'mongodb://192.168.90.45/newTest'
-	// host = 'mongodb://192.168.90.45/prepare'
+	host = 'mongodb://192.168.90.45/prepare'
 	// host = 'mongodb://127.0.0.1/test'
 
 } else if (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'testing') {
 	host = 'mongodb://root:xeJE0jWU@localhost/test'
 } else {
-	host = 'mongodb://192.168.90.45/test'
+	// host = 'mongodb://192.168.90.45/test'
+	host = 'mongodb://192.168.90.45/newTest'
 }
 
 module.exports = exports = host
