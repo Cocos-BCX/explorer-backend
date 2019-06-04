d22486b2ddb8284cb42261e4fd31c350cd791aab
ypenghui6 <ypenghui6@163.com>
2019-06-04 10:28:59 +0800

    add new test db

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
 let host
 if (process.env.NODE_ENV === 'released' || process.env.NODE_ENV === 'release') {
-	host = 'mongodb://192.168.90.45/prepare'
+	host = 'mongodb://192.168.90.45/newTest'
+	// host = 'mongodb://192.168.90.45/prepare'
 	// host = 'mongodb://127.0.0.1/test'
 
 } else if (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'testing') {
-	// host = 'mongodb://root:xeJE0jWU@localhost/test'
+	host = 'mongodb://root:xeJE0jWU@localhost/test'
 } else {
 	host = 'mongodb://192.168.90.45/test'
 }
